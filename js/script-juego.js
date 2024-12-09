f324c30ae397b8bacc4399317062cd99fe87a884
PerezGero <geroperezcollazo@gmail.com>
2024-12-09 16:31:20 -0300

    rehacer

--- js/script-juego.js
+++ js/script-juego.js
@@ -1,3 +1,4 @@
+// Creo un Array de questions con 3 elemntos cada uno, la pregunta en si, 3 opciones, y al final agrego la opcion correcta en un valor entre 0 y 2.
 const questions = [
   {
     question: "¿Cuál es el habitat del UE-02 Pheromosa?",
@@ -26,9 +27,13 @@ const questions = [
   }
 ];
 
+// declaro variables como los puntos que va adquiriendo el jugador, el nombre de dicho jugador y el numero de preunta por la que va.
+
 let currentQuestion = 0;
 let score = 0;
 let playerName = "";
+
+// con el addeventlistener y el documentgetbyid tomo los elementos del DOM para anclarlos a mis constantes de mismo nombre.
 
 document.addEventListener("DOMContentLoaded", () => {
   const startButton = document.getElementById("startButton");
@@ -41,6 +46,10 @@ document.addEventListener("DOMContentLoaded", () => {
   const finalMessageDiv = document.getElementById("finalMessage");
   const playerNameInput = document.getElementById("playerName");
 
+// al hacer click en e boton de start se ejecuta el trim para evitar que la persona ponga espacios antes de su nombre o despues.
+// y tmbn un alert en caso de que no haya puesto ningun valor
+// cuando lo del nombre se cumple lo que hace es empezar a mostrar as preguntas, removiendo el espacio que ocupaba el gamediv agregandole la class hidden.
+
   startButton.addEventListener("click", () => {
     playerName = playerNameInput.value.trim();
     if (playerName === "") {
@@ -52,7 +61,13 @@ document.addEventListener("DOMContentLoaded", () => {
     showQuestion();
   });
 
+// funcion para anclar el click a la funcion de restartgame.
+
   restartButton.addEventListener("click", restartGame);
+
+// la funcion que muestra las preguntas, verificando el numero de la pregunta en el momento y si es menor al valor total del array.
+// usando el foreach realizo la funcion de buscar en el array las opciones y crear un boton con cada una y despues al hacer cick en una de esas se verifica con el checkanswer.
+// para despues crear un elemento hijo, que sea un boton.
 
   function showQuestion() {
     if (currentQuestion < questions.length) {
@@ -70,6 +85,8 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   }
 
+// si selecciono la respuesta correcta se le suma un valor a mi score y si no se suma uno al currentquestion y se sigue mostrando la siguiente pregunta.
+
   function checkAnswer(selected) {
     if (selected === questions[currentQuestion].answer) {
       score++;
@@ -77,6 +94,8 @@ document.addEventListener("DOMContentLoaded", () => {
     currentQuestion++;
     showQuestion();
   }
+
+// la funcion de endgame se invoca al momento en el que la pregunta del momento iguala o supera el valor del lenght del array y hace aparecer un mensaje e imagen si la persona llego a los 5 puntos, pero si no le aparce otro msj sin imagen.
 
   function endGame() {
     quizDiv.classList.add("hidden");
@@ -88,6 +107,8 @@ document.addEventListener("DOMContentLoaded", () => {
       finalMessageDiv.innerHTML = `Lo lamento ${playerName}, obtuviste ${score} puntos. ¡Intenta nuevamente!`;
     }
   }
+
+// se reinicia todo el juego y todo vuelve a su estado originas de display none y los valores se resetean.
 
   function restartGame() {
     currentQuestion = 0;
